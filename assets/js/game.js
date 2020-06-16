225c076202dbddde374c5a8833dc6925add4e34b
Waves <64664814+sanders43@users.noreply.github.com>
2020-06-15 23:19:20 -0500

    if else statements

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,9 +5,14 @@ let playerAttack = 10;
 let enemyName = "Roborto";
 let enemyHealth = 50;
 let enemyAttack = 12;
+let promptFight = "";
+let playerMoney = 10;
+let confirmSkip = "";
 
 let fight = function() {
     window.alert("Welcome to Robot Gladiators!");
+    promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose!").toLowerCase();
+    if (promptFight === "fight") {
     enemyHealth = (enemyHealth - playerAttack);
     console.log(`${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health remaining.`);
     if (enemyHealth <= 0) {
@@ -18,10 +23,22 @@ let fight = function() {
     playerHealth = (playerHealth - enemyAttack);
     console.log(`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health remaining.`);
     if (playerHealth <= 0) {
-        window.alert(`${playerName} has died!`) 
+        window.alert(`${playerName} has died!`); 
     } else {
-        window.alert(`${playerName} still has ${playerHealth} health left.`)
+        window.alert(`${playerName} still has ${playerHealth} health left.`);
     }
+} else if (promptFight === "skip") {
+    confirmSkip = window.confirm("Are you sure you'd like to skip? There is a penalty.");
+    if(confirmSkip) {
+        window.alert(`${playerName} has decided to skip this fight.`);
+        playerMoney = (playerMoney - 2);
+    } else { 
+        fight();
+    }
+} else {
+    window.alert("You need to choose a valid option. Try again!");
+}
+
     
 }
 
