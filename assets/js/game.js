aa015096e795fcddc915e3d901e2b814fd5e1db0
Waves <64664814+sanders43@users.noreply.github.com>
2020-06-15 22:45:38 -0500

    fight function start

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,28 @@
-let playerName = window.prompt("What is your robots name?")
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-console.log(10+10);
-console.log(`Our robot's name is ${playerName}`);
+let playerName = window.prompt("What is your robot's name?");
+let playerHealth = 100;
+let playerAttack = 10;
 
-//this creates a function named fight
-function fight() {
-    window.alert("The fight has begun!")
+let enemyName = "Roborto";
+let enemyHealth = 50;
+let enemyAttack = 12;
+
+let fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+    enemyHealth = (enemyHealth - playerAttack);
+    console.log(`${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health remaining.`);
+    if (enemyHealth <= 0) {
+        window.alert(`${enemyName} has died!`);
+    } else {
+        window.alert(`${enemyName} still has ${enemyHealth} health left.`);
+    }
+    playerHealth = (playerHealth - enemyAttack);
+    console.log(`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health remaining.`);
+    if (playerHealth <= 0) {
+        window.alert(`${playerName} has died!`) 
+    } else {
+        window.alert(`${playerName} still has ${playerHealth} health left.`)
+    }
+    
 }
-//fight();
+
+fight();
