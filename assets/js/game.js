2608b3b79ce5162cdd05d4270a2bea709d28d01b
Waves <64664814+sanders43@users.noreply.github.com>
2020-06-15 22:15:52 -0500

    variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("this is an alert! Javascript is running!")
+let playerName = window.prompt("What is your robots name?")
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10+10);
+console.log(`Our robot's name is ${playerName}`);
+
+//this creates a function named fight
+function fight() {
+    window.alert("The fight has begun!")
+}
+//fight();
