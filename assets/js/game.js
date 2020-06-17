38ca61cb51dcec76ad606833e7f26011c0301052
Waves <64664814+sanders43@users.noreply.github.com>
2020-06-17 13:40:27 -0500

    shop feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,7 +4,7 @@ let playerAttack = 10;
 let enemyHealth = 50;
 let enemyAttack = 12;
 let promptFight = "";
-let playerMoney = 10;
+let playerMoney = 100;
 let confirmSkip = "";
 let pickedEnemyName = "";
 let enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
@@ -16,7 +16,7 @@ let fight = function(enemyName) {
         confirmSkip = window.confirm("Are you sure you'd like to skip? There is a penalty.");
         if(confirmSkip) {
             window.alert(`${playerName} has decided to skip this fight.`);
-            playerMoney = (playerMoney - 10);
+            playerMoney -= 10;
             console.log(`playerMoney, ${playerMoney}`)
             break;
         }
@@ -44,7 +44,7 @@ let fight = function(enemyName) {
  const startGame = function() {
      playerHealth = 100;
      playerAttack = 10;
-     playerMoney = 10;
+     playerMoney = 100;
 
 for(var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
@@ -55,6 +55,13 @@ for(var i = 0; i < enemyNames.length; i++) {
     enemyHealth = 50;
 
     fight(pickedEnemyName);
+    if(playerHealth > 0 && i < enemyNames.length - 1) {
+        let storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+        if(storeConfirm) {
+        shop();
+    }
+};
+
 } else {
     window.alert("You have lost your robot in battle! Game over!");
     break;
@@ -65,7 +72,7 @@ for(var i = 0; i < enemyNames.length; i++) {
 
  const endGame = () => {
      if (playerHealth > 0) { 
-         window.alert(`Great job, you've survied the game! You now have a socre of ${playerMoney}.`);
+         window.alert(`Great job, you've survied the game! You now have a score of ${playerMoney}.`);
      } else {
          window.alert("You have lost your robot in battle.");
      }
@@ -76,6 +83,40 @@ for(var i = 0; i < enemyNames.length; i++) {
  } else {
      window.alert("Thank you for playing Robot Gladiators!");
  }
+};
+
+const shop = () => {
+let shopOptionPrompt = window.prompt(`You have $${playerMoney}. Would you like to REFILL your health by 20 for $7, UPGRADE your attack by 6 for 7$, or LEAVE the store? Please choose one: REFILL, UPGRADE, or LEAVE.`).toLowerCase();
+switch(shopOptionPrompt) {
+    case "refill":
+        if (playerMoney >= 7 ) {
+        window.alert("Refilling player's health by 20 for $7")
+        playerHealth += 20;
+        playerMoney -= 7;
+        window.alert(`${playerName} now has ${playerHealth} health and $${playerMoney}.`) }
+        else {
+            window.alert("You dont have enough money!");
+        }
+        break;
+    case "upgrade":
+        if(playerMoney >= 7) {
+        window.alert(`Increasing ${playerName}'s attack by 6 for $7`)
+        playerAttack += 6;
+        playerMoney -= 7; 
+        window.alert(`${playerName} now has ${playerAttack} attack and $${playerMoney}.`)}
+        else {
+            window.alert("You dont have enough money!")
+        }
+        break;
+    case "leave":
+       window.alert("Leaving the store!")
+       break;
+    default:
+        window.alert("You did not pick a valid option, try again!")
+        shop();
+        break;
+
+}
 };
 
  startGame();
